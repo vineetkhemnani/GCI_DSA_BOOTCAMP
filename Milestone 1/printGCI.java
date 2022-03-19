5f351dd8d79e92baffb37404f8ddbd6349d6d7a8
vineetkhemnani <vineetkhemnani09@gmail.com>
2022-03-19 15:36:17 +0530

    new changes with comments

--- Milestone 1/printGCI.java
+++ Milestone 1/printGCI.java
@@ -1,5 +1,15 @@
 public class printGCI{
+
+    // public--> so that program can be read by JDK anywhere
+    // class--> keyword that creates a new class
+    // printGCI--> name of the class
     public static void main(String[] args) {
+        // static-->
+        // void--> no return type
+        //main()--> entry point where the program starts
         System.out.println("Welcome to Girl Code It");
+        // System--> keyword for pre-defined class that provides access to system
+        // out-->output stream access
+        // println()--> prints output and changes line afterwards
     }
 }
